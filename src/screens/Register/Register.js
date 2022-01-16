98f77bb6af3aee24c78408d1099bcc064581fd8a
Jeel Patel <jeel@toddleapp.com>
2022-01-16 17:16:42 +0530

    feat: integrate register page with backend

--- src/screens/Register/Register.js
+++ src/screens/Register/Register.js
@@ -1,5 +1,6 @@
 /**--external-- */
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 /**--internal-- */
 import { AuthenticationPage } from '../../components';
@@ -7,14 +8,41 @@ import { AuthenticationPage } from '../../components';
 /**--relative-- */
 import { formFields } from './utils';
 
+const origin = 'http://localhost:4000';
+
 const Register = () => {
+  const navigate = useNavigate();
+
+  const submitForm = async (data) => {
+    try {
+      const res = await fetch(`${origin}/signup`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        referrerPolicy: 'no-referrer',
+      });
+
+      const responseData = await res.json();
+
+      const { success } = responseData;
+
+      if (success) {
+        navigate('/login');
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <AuthenticationPage
       headingText="Sign up"
       linkButtonText="Sign in"
       submitButtonText="Sign up"
       formFields={formFields}
-      onSubmit={(data) => console.log(data)}
+      onSubmit={submitForm}
       linkButtonHref="/login"
     />
   );
