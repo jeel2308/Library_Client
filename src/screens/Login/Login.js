b1f7f9d36383cfb601dcddd7695086283cf1089e
Jeel Patel <jeel@toddleapp.com>
2022-01-16 18:12:22 +0530

    feat: integrate login page with backend

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,20 +1,47 @@
 /**--external-- */
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 /**--internal-- */
 import { AuthenticationPage } from '../../components';
+import { setUserInfoInStorage } from '../../Utils';
 
 /**--relative-- */
 import { formFields } from './utils';
 
+const origin = 'http://localhost:4000';
+
 const Login = () => {
+  const navigate = useNavigate();
+  const submitForm = async (data) => {
+    try {
+      const res = await fetch(`${origin}/login`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        referrerPolicy: 'no-referrer',
+      });
+      const responseData = await res.json();
+      const { success, ...userInfo } = responseData;
+      if (success) {
+        setUserInfoInStorage({ userInfo });
+        navigate('/resources');
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+    }
+  };
+
   return (
     <AuthenticationPage
       formFields={formFields}
       submitButtonText="Sign in"
       linkButtonText="Create account"
       headingText="Sign in"
-      onSubmit={(data) => console.log(data)}
+      onSubmit={submitForm}
       linkButtonHref="/register"
     />
   );
