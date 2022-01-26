5aaf6193df8840a7e1f307a0d9e0e2ad46eabfbc
Jeel Patel <jeel@toddleapp.com>
2022-01-26 21:13:44 +0530

    feat: add thunk for login and sign up

--- src/modules/Module.js
+++ src/modules/Module.js
@@ -1,3 +1,8 @@
+import _isEmpty from 'lodash/isEmpty';
+import { setUserInfoInStorage } from '../Utils';
+
+const origin = process.env.REACT_APP_SERVER_URL;
+
 const SET_LOADER_VISIBILITY = 'SET_LOADER_VISIBILITY';
 
 const UPDATE_USER_LOGGED_IN_STATUS = 'UPDATE_USER_LOGGED_IN_STATUS';
@@ -20,6 +25,121 @@ export const setUserDetails = (payload) => {
 
 export const setToastMessage = (payload) => {
   return { type: SET_TOAST_MESSAGE, payload };
+};
+
+export const loginUser = (data) => {
+  return async (dispatch, getState) => {
+    let responseData = {};
+
+    let res = {};
+    try {
+      dispatch(setLoaderVisibility(true));
+
+      res = await fetch(`${origin}/login`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        referrerPolicy: 'no-referrer',
+      });
+
+      responseData = await res.json();
+
+      const { message, ...userInfo } = responseData;
+      if (res.ok) {
+        setUserInfoInStorage({ userInfo });
+        dispatch(setUserDetails(userInfo));
+
+        /**
+         * When we redirect using following approach, it will unmount App and
+         * remount it. This is necessary because without it App won't be able to
+         * use updated localStorage data.
+         */
+        window.location.href = '/';
+      } else {
+        dispatch(
+          setToastMessage({
+            title: message || res.statusText,
+            status: 'error',
+            isClosable: true,
+            position: 'bottom-left',
+          })
+        );
+      }
+    } catch (e) {
+      dispatch(
+        setToastMessage({
+          title: 'Something went wrong',
+          status: 'error',
+          isClosable: true,
+          position: 'bottom-left',
+        })
+      );
+    } finally {
+      setLoaderVisibility(false);
+    }
+  };
+};
+
+export const registerUser = (data, successCallback) => {
+  return async (dispatch) => {
+    let responseData = {};
+
+    let res = {};
+
+    try {
+      dispatch(setLoaderVisibility(true));
+
+      res = await fetch(`${origin}/signup`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        referrerPolicy: 'no-referrer',
+      });
+
+      /**
+       * If res status is ok(status is 200) then we will do redirection
+       */
+      if (res.ok) {
+        successCallback && successCallback();
+      } else {
+        /**
+         * Fetch api throws error only when network error occur.
+         * For status 4xx and 5xx, we have to add logic for toast in try block
+         */
+        responseData = await res.json();
+
+        const { message } = responseData;
+
+        /**
+         * If there is message from backend then we will use it otherwise we use
+         * default failure message from res obj.
+         */
+        dispatch(
+          setToastMessage({
+            title: message || res.statusText,
+            status: 'error',
+            isClosable: true,
+            position: 'bottom-left',
+          })
+        );
+      }
+    } catch (e) {
+      dispatch(
+        setToastMessage({
+          title: 'Something went wrong',
+          status: 'error',
+          isClosable: true,
+          position: 'bottom-left',
+        })
+      );
+    } finally {
+      dispatch(setLoaderVisibility(false));
+    }
+  };
 };
 
 const reducerHandlers = {
@@ -49,10 +169,9 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
-  const type = action?.type ?? '';
-  if (!type) return initialState;
+  const type = action.type;
   const stateHandler = reducerHandlers[type];
-  return stateHandler?.(state, action) ?? state;
+  return stateHandler?.(state, action) ?? initialState;
 };
 
 export default reducer;
