314d8f7892c03dcdc3b8251e8c525b0b2422cd4d
Jeel Patel <jeel@toddleapp.com>
2022-01-26 21:10:41 +0530

    chore: move logic of toast message to seperate component

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Form from './Form';
 import AuthenticationPage from './AuthenticationPage';
 import Modal from './Modal';
 import FullScreenLoader from './FullScreenLoader';
-export { Form, Modal, AuthenticationPage, FullScreenLoader };
+import ToastMessage from './ToastMessage';
+export { Form, Modal, AuthenticationPage, FullScreenLoader, ToastMessage };
