f5b10ae8e07866a92845e4da63a0f1f379a8bcaf
Jeel Patel <jeel@toddleapp.com>
2022-02-13 14:43:01 +0530

    chore: remove Modal

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -9,14 +9,10 @@ import {
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-/**--internal-- */
-import { Modal } from '../../components';
-
 /**--relative-- */
 import classes from './Home.module.scss';
 import { buttonGroupStyles } from './HomeStyles';
 import {
-  HOME_BANNER,
   TITLE_ELEMENT,
   TITLE_FONT_SIZE,
   LIST_FONT_SIZE,
@@ -26,43 +22,38 @@ import {
 const Home = () => {
   return (
     <div className={classes.container}>
-      <figure className={classes.figure}>
-        <img src={HOME_BANNER} alt="home-banner" />
-      </figure>
-      <Modal>
-        <div className={classes.overlay}>
-          <ButtonGroup spacing={'6'} style={buttonGroupStyles}>
-            <Link to={'register'}>
-              <Button colorScheme={'blue'}>Sign up</Button>
-            </Link>
-            <Link to={'login'}>
-              <Button colorScheme={'blue'}>Sign in</Button>
-            </Link>
-          </ButtonGroup>
-          <div className={classes.content}>
-            <Heading as={TITLE_ELEMENT} size={TITLE_FONT_SIZE}>
-              One place to keep all online articles
-            </Heading>
-            <UnorderedList mt={'6'}>
-              <ListItem>
-                <Heading as={LIST_ITEM_ELEMENT} size={LIST_FONT_SIZE}>
-                  Manage articles easily
-                </Heading>
-              </ListItem>
-              <ListItem>
-                <Heading as={LIST_ITEM_ELEMENT} size={LIST_FONT_SIZE} mt={'2'}>
-                  Group articles by subject
-                </Heading>
-              </ListItem>
-              <ListItem>
-                <Heading as={LIST_ITEM_ELEMENT} size={LIST_FONT_SIZE} mt={'2'}>
-                  Track articles status
-                </Heading>
-              </ListItem>
-            </UnorderedList>
-          </div>
+      <div className={classes.content}>
+        <ButtonGroup spacing={'6'} style={buttonGroupStyles}>
+          <Link to={'register'}>
+            <Button colorScheme={'blue'}>Sign up</Button>
+          </Link>
+          <Link to={'login'}>
+            <Button colorScheme={'blue'}>Sign in</Button>
+          </Link>
+        </ButtonGroup>
+        <div className={classes.heroContainer}>
+          <Heading as={TITLE_ELEMENT} size={TITLE_FONT_SIZE}>
+            One place to keep all online articles
+          </Heading>
+          <UnorderedList mt={'6'}>
+            <ListItem>
+              <Heading as={LIST_ITEM_ELEMENT} size={LIST_FONT_SIZE}>
+                Manage articles easily
+              </Heading>
+            </ListItem>
+            <ListItem>
+              <Heading as={LIST_ITEM_ELEMENT} size={LIST_FONT_SIZE} mt={'2'}>
+                Group articles by subject
+              </Heading>
+            </ListItem>
+            <ListItem>
+              <Heading as={LIST_ITEM_ELEMENT} size={LIST_FONT_SIZE} mt={'2'}>
+                Track articles status
+              </Heading>
+            </ListItem>
+          </UnorderedList>
         </div>
-      </Modal>
+      </div>
     </div>
   );
 };
