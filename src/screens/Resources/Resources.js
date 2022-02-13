8f56c30980b3c6f68b7ce945f75c95ee99123daa
Jeel Patel <jeel@toddleapp.com>
2022-02-13 19:29:00 +0530

    feat: integrate search and add folder features

--- src/screens/Resources/Resources.js
+++ src/screens/Resources/Resources.js
@@ -1,32 +1,68 @@
 /**--external-- */
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { connect } from 'react-redux';
 import _isEmpty from 'lodash/isEmpty';
 import _get from 'lodash/get';
 import _map from 'lodash/map';
 import _pipe from 'lodash/flow';
+import _filter from 'lodash/filter';
+import _includes from 'lodash/includes';
 
 /**--internal-- */
-import { compose } from '../../Utils';
+import { compose, getMatchingResults } from '../../Utils';
 import { withQuery, Sidebar } from '../../components';
 import { getUserFoldersQuery } from '../../modules/Queries';
 
 /**--relative-- */
 import classes from './Resources.module.scss';
+import AddButton from './AddButton';
+import Search from './Search';
+import EditOrCreateFolderModal from './EditOrCreateFolderModal';
 import { loadingContainerStyle } from './ResourcesStyles';
 
 const Resources = (props) => {
   const { folders } = props;
 
+  const [searchValue, setSearchValue] = useState('');
+
+  const [showEditOrCreateFolderModal, setShowEditOrCreateFolderModal] =
+    useState(false);
+
+  const closeEditOrCreateFolderModal = useCallback(() => {
+    setShowEditOrCreateFolderModal(false);
+  }, []);
+
+  const matchingFolders = getMatchingResults({
+    list: folders,
+    field: 'label',
+    searchText: searchValue,
+  });
+
   return (
     <div className={classes.container}>
-      <div className={classes.sidebarContainer}>
-        <Sidebar
-          initialActiveOption={folders[0]?.id}
-          sidebarOptions={folders}
-          onClickOption={(args) => console.log(args)}
-        />
+      <div className={classes.leftContainer}>
+        <div className={classes.header}>
+          <Search value={searchValue} onChange={setSearchValue} />
+          <AddButton
+            tooltipLabel="Create folder"
+            onClick={() => setShowEditOrCreateFolderModal(true)}
+          />
+        </div>
+        <div className={classes.sidebarContainer}>
+          {!_isEmpty(matchingFolders) ? (
+            <Sidebar
+              initialActiveOption={folders[0]?.id}
+              sidebarOptions={matchingFolders}
+              onClickOption={(args) => console.log(args)}
+            />
+          ) : (
+            <div className={classes.noMatchText}>{'No match found'}</div>
+          )}
+        </div>
       </div>
+      {showEditOrCreateFolderModal && (
+        <EditOrCreateFolderModal closeModal={closeEditOrCreateFolderModal} />
+      )}
     </div>
   );
 };
