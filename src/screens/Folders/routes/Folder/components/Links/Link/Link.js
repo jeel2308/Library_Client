a1a9d3109103f90bd72b8fffc06d7b4ef722ff8f
Jeel Patel <jeel@toddleapp.com>
2022-03-01 18:44:58 +0530

    feat: add UI for metadata

--- src/screens/Folders/routes/Folder/components/Links/Link/Link.js
+++ src/screens/Folders/routes/Folder/components/Links/Link/Link.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BiDotsVerticalRounded } from 'react-icons/bi';
 import { IconContext } from 'react-icons';
+import { Heading, Text } from '@chakra-ui/react';
 
 /**--internal-- */
 import { Dropdown } from '#components';
@@ -10,31 +11,71 @@ import { Dropdown } from '#components';
 import classes from './Link.module.scss';
 import { dotsStyle } from './LinkStyles';
 
+const Metadata = (props) => {
+  const { title, description, thumbnail } = props;
+  return (
+    <div className={classes.linkMetadataContainer}>
+      {title && (
+        <Heading as="h5" size={'md'}>
+          {title}
+        </Heading>
+      )}
+      {description && (
+        <Text fontSize={'sm'} colorScheme="blackAlpha">
+          {description}
+        </Text>
+      )}
+      {thumbnail && (
+        <figure className={classes.thumbnail}>
+          <img src={thumbnail} alt={title} />
+        </figure>
+      )}
+    </div>
+  );
+};
+
 const Link = (props) => {
-  const { url, dropDownOptions, id } = props;
+  const { url, dropDownOptions, title, thumbnail, description, id } = props;
 
   const handleActions = ({ value }) => {
     props.handleActions({ value, linkId: id });
   };
 
+  const onClick = (e) => {
+    if (e.defaultPrevented) {
+      return;
+    }
+    window.open(url, '_blank');
+  };
+
+  const showMetadata = title || thumbnail || description;
+
   return (
-    <div className={classes.container}>
-      <a href={url} target={'_blank'} className={classes.link}>
-        {url}
-      </a>
-      <div className={classes.dropDownContainer}>
-        <Dropdown
-          variant="unstyled"
-          options={dropDownOptions}
-          dropdownButtonType="icon"
-          handleActions={handleActions}
-          icon={
-            <IconContext.Provider value={dotsStyle}>
-              <BiDotsVerticalRounded />
-            </IconContext.Provider>
-          }
-        />
+    <div className={classes.container} onClick={onClick}>
+      <div className={classes.firstRow}>
+        <Text className={classes.link}>{url}</Text>
+
+        <div className={classes.dropDownContainer}>
+          <Dropdown
+            variant="unstyled"
+            options={dropDownOptions}
+            dropdownButtonType="icon"
+            handleActions={handleActions}
+            icon={
+              <IconContext.Provider value={dotsStyle}>
+                <BiDotsVerticalRounded />
+              </IconContext.Provider>
+            }
+          />
+        </div>
       </div>
+      {showMetadata && (
+        <Metadata
+          title={title}
+          description={description}
+          thumbnail={thumbnail}
+        />
+      )}
     </div>
   );
 };
