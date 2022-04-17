df4966a749595056cdd25ee3e5af541d453fcb77
Jeel Patel <jeel@toddleapp.com>
2022-04-17 15:23:16 +0530

    feat(ChangePassword): integrate with change password thunk

--- src/screens/Authentication/routes/ChangePassword/components/ChangePassword.js
+++ src/screens/Authentication/routes/ChangePassword/components/ChangePassword.js
@@ -1,25 +1,50 @@
 /**--external-- */
 import React from 'react';
+import { connect } from 'react-redux';
+import _get from 'lodash/get';
+import { useNavigate } from 'react-router-dom';
 
 /**--internal-- */
 import { AuthenticationPage } from '#components';
+import { changePassword } from '#modules/Module';
 
 /**--relative-- */
 import { formFields } from './ChangePasswordUtils';
 
-const ChangePassword = () => {
+const ChangePassword = (props) => {
+  const { userId, changePassword } = props;
+
+  const navigate = useNavigate();
+
+  const onChangePassword = () => {
+    navigate('/login');
+  };
+
+  const onSubmit = (data) => {
+    changePassword({ ...data, id: userId }, onChangePassword);
+  };
+
   return (
     <AuthenticationPage
       formFields={formFields}
       submitButtonText="Change password"
       linkButtonText="Sign in"
       linkButtonHref="/login"
-      onSubmit={console.log}
+      onSubmit={onSubmit}
       headingText="Change password"
     />
   );
 };
 
-export default ChangePassword;
+const mapStateToProps = (state) => {
+  const userId = _get(state, 'userDetails.id', null);
+  return { userId };
+};
+
+const mapActionCreators = {
+  changePassword,
+};
+
+export default connect(mapStateToProps, mapActionCreators)(ChangePassword);
 
 ChangePassword.displayName = 'ChangePassword';
